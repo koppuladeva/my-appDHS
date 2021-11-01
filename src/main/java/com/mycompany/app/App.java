78d852256d003723b0254329ae1bb0c1aba6d9c5
koppuladeva <57366811+koppuladeva@users.noreply.github.com>
2021-11-01 18:04:08 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello DHS!" );
-	System.out.printn( " This is Deva's Demo ");
+	System.out.println( " This is Deva's Demo ");
     }
 }
